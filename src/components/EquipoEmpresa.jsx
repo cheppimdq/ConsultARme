ebca1c3cf4da1ee30ee6f7095ca217c1d52755c7
cheppimdq <cheppimdp@gmail.com>
2024-04-28 18:22:58 -0300

    empresa

--- src/components/EquipoEmpresa.jsx
+++ src/components/EquipoEmpresa.jsx
@@ -3,53 +3,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const EquipoEmpresa = () => {
     return (
-
-
         <>
             <main className="container mt-4">
-                <div className="row link-negro">
-
-                    <div className="row justify-content-center mb-4">
-                        <div className="col-md-7 text-center">
-                            <h3 className="mb-3">Experienced & Professional Team</h3>
-                            <h6 className="subtitle">You can rely on our amazing features list and also our customer services will be a great experience for you without a doubt and in no time</h6>
+                <div className="row link-negro d-flex justify-content-evenly">
+                    <div className="col-md-3 text-center">
+                        <img src="/ConsultARme/victoria-1.jpg" alt="wrapkit" className="img-fluid rounded-circle" />
+                        <div className="pt-2">
+                            <h5 className="mt-2 font-weight-medium mb-0">Victoria Callegher</h5>
+                            <ul className="list-inline mt-1">
+                                <li className="list-inline-item"><a className="nav-link boton-in" href="https://www.linkedin.com/in/victoria-vanesa-callegher-a18880b9/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin-in']} className='icono-in' /></a></li>
+                                <li className="list-inline-item"><a className="nav-link" href="https://www.linkedin.com/in/victoria-vanesa-callegher-a18880b9/" target="_blank" rel="noopener noreferrer"><h6 className="subtitle mb-3">Socia en ConsultARme</h6></a></li>
+                            </ul>
+                            <p className='mt-2'>Capacitación e implementación de Sistema Tango Gestión, soporte al cliente vía mesa de ayuda y supervisión de equipo técnico.</p>
                         </div>
                     </div>
-                    <div className="row">
-                        {/* column  */}
-                        <div className="col-lg-3 mb-4">
-                            <div className="row">
-                                <div className="col-md-12 text-center">
-                                    <img src="/ConsultARme/victoria-1.jpg" alt="wrapkit" className="img-fluid rounded-circle" />
-                                    <div className="pt-2">
-                                        <h5 className="mt-2 font-weight-medium mb-0">Victoria Vanesa Callegher</h5>
-                                        <ul className="list-inline mt-1">
-                                            <li className="list-inline-item"><a className="nav-link boton-in" href="https://www.linkedin.com/company/consultarme/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin-in']} className='icono-in'/></a></li>
-                                            <li className="list-inline-item"><a className="nav-link" href="https://www.linkedin.com/company/consultarme/" target="_blank" rel="noopener noreferrer"><h6 className="subtitle mb-3">Socia en ConsultARme</h6>
-                                            </a></li>
-                                        </ul>
-                                        <p className='mt-2'>Capacitación e implementación de Sistema Tango Gestión, soporte al cliente vía mesa de ayuda y supervisión de equipo técnico.</p>
-                                    </div>
-                                </div>
-                            </div>
+
+                    <div className="col-md-3 text-center">
+                        <img src="/ConsultARme/gus-1.jpg" alt="wrapkit" className="img-fluid rounded-circle" />
+                        <div className="pt-2">
+                            <h5 className="mt-2 font-weight-medium mb-0">Gustavo Boye</h5>
+                            <ul className="list-inline mt-1">
+                                <li className="list-inline-item"><a className="nav-link boton-in" href="https://www.linkedin.com/in/gustavo-boye-80712772/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin-in']} className='icono-in' /></a></li>
+                                <li className="list-inline-item"><a className="nav-link" href="https://www.linkedin.com/in/gustavo-boye-80712772/" target="_blank" rel="noopener noreferrer"><h6 className="subtitle mb-3">Socio en ConsultARme</h6></a></li>
+                            </ul>
+                            <p className='mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio neque et, alias culpa laboriosam inventore.</p>
                         </div>
-                        {/* column  */}
-
-                        {/* Repeat the above column structure for each team member */}
-
                     </div>
-
-
-
+                    
                 </div>
-
             </main>
         </>
-
-
-
-
-
-
     );
 };
