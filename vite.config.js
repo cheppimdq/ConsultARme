cbf4b9e5c03b0288e5ae7f93b375a5dac01673df
cheppimdq <cheppimdp@gmail.com>
2024-04-28 17:09:44 -0300

    - chunks

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ConsultARme/',
-  build: {
-    chunkSizeWarningLimit: 1000, // Ajustar el límite de advertencia de tamaño de fragmento
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // Agrupar módulos de node_modules en un fragmento separado
-          }
-        },
-      },
-    },
-  },
+  base: '/ConsultARme/'
 })
