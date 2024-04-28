401de9070f33537e04e2c5e27f0c9596a27a8769
cheppimdq <cheppimdp@gmail.com>
2024-04-28 18:06:24 -0300

    linkedin

--- src/components/EquipoEmpresa.jsx
+++ src/components/EquipoEmpresa.jsx
@@ -9,54 +9,36 @@ export const EquipoEmpresa = () => {
             <main className="container mt-4">
                 <div className="row link-negro">
 
-                    {/* <div className="col-md-4 p-3">
-                        <img className="empresa-perfil" src="/vic.jpeg" alt="Foto Perfil" />
-                        <h3 className="text-center">Victoria Vanesa Callegher</h3>
-                        <div className="flex-pp">
-                            <a className="boton-in" href="https://www.linkedin.com/in/jor-amoreo-casotti/" target="_blank">
-                                <FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
-                            </a>
-                            <p className="mb-0 mx-2">Socia y Asistente en ConsultARme Argentina</p>
-                        </div>
-                    </div> */}
-
-                    <div className="py-5 team4">
-                        <div className="container">
-                            <div className="row justify-content-center mb-4">
-                                <div className="col-md-7 text-center">
-                                    <h3 className="mb-3">Experienced & Professional Team</h3>
-                                    <h6 className="subtitle">You can relay on our amazing features list and also our customer services will be great experience for you without doubt and in no-time</h6>
-                                </div>
-                            </div>
-                            <div className="row">
-                                {/* column */}
-                                <div className="col-lg-3 mb-4">
-                                    {/* Row */}
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            <img src="/ConsultARme/victoria-1.jpg" alt="wrapkit" className="img-fluid rounded-circle" />
-                                        </div>
-                                        <div className="col-md-12 text-center">
-                                            <div className="pt-2">
-                                                <h5 className="mt-4 font-weight-medium mb-0">Michael Doe</h5>
-                                                <h6 className="subtitle mb-3">Property Specialist</h6>
-                                                <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
-                                                <ul className="list-inline">
-                                                    <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-facebook"></i></a></li>
-                                                    <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-twitter"></i></a></li>
-                                                    <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-instagram"></i></a></li>
-                                                    <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-behance"></i></a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* Row */}
-                                </div>
-                                {/* column */}
-                                {/* Repeat the above column code for other team members */}
-                            </div>
+                    <div className="row justify-content-center mb-4">
+                        <div className="col-md-7 text-center">
+                            <h3 className="mb-3">Experienced & Professional Team</h3>
+                            <h6 className="subtitle">You can rely on our amazing features list and also our customer services will be a great experience for you without a doubt and in no time</h6>
                         </div>
                     </div>
+                    <div className="row">
+                        {/* column  */}
+                        <div className="col-lg-3 mb-4">
+                            <div className="row">
+                                <div className="col-md-12 text-center">
+                                    <img src="/ConsultARme/victoria-1.jpg" alt="wrapkit" className="img-fluid rounded-circle" />
+                                    <div className="pt-2">
+                                        <h5 className="mt-2 font-weight-medium mb-0">Victoria Vanesa Callegher</h5>
+                                        <ul className="list-inline mt-1">
+                                            <li className="list-inline-item"><a className="nav-link boton-in" href="https://www.linkedin.com/company/consultarme/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin-in']} className='icono-in'/></a></li>
+                                            <li className="list-inline-item"><a className="nav-link" href="https://www.linkedin.com/company/consultarme/" target="_blank" rel="noopener noreferrer"><h6 className="subtitle mb-3">Socia en ConsultARme</h6>
+                                            </a></li>
+                                        </ul>
+                                        <p className='mt-2'>Capacitación e implementación de Sistema Tango Gestión, soporte al cliente vía mesa de ayuda y supervisión de equipo técnico.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* column  */}
+
+                        {/* Repeat the above column structure for each team member */}
+
+                    </div>
+
 
 
                 </div>
--- src/components/FooterNegro.jsx
+++ src/components/FooterNegro.jsx
@@ -14,9 +14,10 @@ export const FooterNegro = () => {
                         <li className="list-inline-item"><NavLink to='/contacto' className="nav-link">Contacto</NavLink></li>
                     </ul>
                     <ul className="nav col-sm-12 col-md-5 flex-footer">
-                    <li className="list-inline-item"><a className="nav-link" href="https://www.instagram.com/consultarme.ok/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'instagram']} /></a></li>
+                        <li className="list-inline-item"><a className="nav-link" href="https://www.instagram.com/consultarme.ok/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'instagram']} /></a></li>
                         <li className="list-inline-item"><a className="nav-link" href="https://www.facebook.com/ConsultARme" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'facebook-f']} /></a></li>
-                        <li className="list-inline-item"><a className="nav-link" href="https://www.linkedin.com/company/consultarme/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></a></li></ul>
+                        <li className="list-inline-item"><a className="nav-link" href="https://www.linkedin.com/company/consultarme/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></a></li>
+                    </ul>
                     <h6 className="col-sm-12 flex-footer">2024 &copy; ConsultARme</h6>
                 </div>
             </footer>
