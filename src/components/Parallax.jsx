038bb978135169c2f696fc9e79b5688a147a9fd1
cheppimdq <cheppimdp@gmail.com>
2024-05-18 18:50:07 -0300

    parallax

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const Parallax1 = () => {
     return (
-        <section>
-            <section class="parallax-pp">
-                <div class="parallax-inner-pp">
-                    <h1>Scroll Down</h1>
+        <section class="parallax-pp">
+            <div class="parallax-inner-pp p-3 text-center">
+                <div className="container py-3">
+                    <div className="row align-items-center justify-content-center my-4">
+                        <div className="col-sm-8">
+                            <img
+                                src="/ConsultARme/logonav.svg"
+                                className="d-block mx-lg-auto"
+                                alt="Bootstrap Themes"
+                                loading="lazy"
+                            />
+                        </div>
+                        <div className="">
+                            <h3 className="display-7 fw-bold lh-1 mb-4">Servicios Tango Delta en forma remota</h3>
+                            <NavLink to='/servicios' className="nav-link"><button className="btn btn-light px-5 mb-5" type="button">
+                                Servicios
+                            </button></NavLink>
+                        </div>
+                    </div>
                 </div>
-            </section>
-            
-            
+            </div>
+
         </section>
+
+
 
     );
 };
