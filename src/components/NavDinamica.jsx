d016865e1c659996fba7e5d86ed9035fde547c32
cheppimdq <cheppimdp@gmail.com>
2024-05-27 12:58:27 -0300

    publicar

--- src/components/NavDinamica.jsx
+++ src/components/NavDinamica.jsx
@@ -17,6 +17,10 @@ export const NavDinamica = () => {
     };
   }, []);
 
+  const handleNavLinkClick = () => {
+    window.scrollTo(0, 0); // Hacer scroll al principio de la página al hacer clic en un enlace
+  };
+
   return (
     <nav className={`navbar navbar-expand-lg fixed-top ${scrolled ? 'nav-blanco' : 'nav-transparente'}`}>
       <div className="container">
@@ -24,13 +28,13 @@ export const NavDinamica = () => {
           <img src={scrolled ? "/marca/nav-celeste.png" : "/marca/nav-blanco.png"} alt="" className="img-fluid foto-pp" style={{ maxHeight: '60px' }} />
         </Link>
         <button className={`navbar-toggler ${scrolled ? 'link-celeste' : 'link-blanco'}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-          <FontAwesomeIcon icon="fa-solid fa-bars" />
+          <FontAwesomeIcon icon={['fas', 'bars']} />
         </button>
         <div className="collapse navbar-collapse" id="navbarScroll">
           <ul className="navbar-nav ms-auto me-0 navbar-nav-scroll">
-            <li className="nav-item px-0 px-lg-4"><NavLink to='/' className={`nav-link ${scrolled ? 'link-celeste' : 'link-blanco'}`}>Inicio</NavLink></li>
-            <li className="nav-item px-0 px-lg-4"><NavLink to='/servicios' className={`nav-link ${scrolled ? 'link-celeste' : 'link-blanco'}`}>Servicios</NavLink></li>
-            <li className="nav-item px-0 px-lg-4"><NavLink to='/contacto' className={`nav-link ${scrolled ? 'link-celeste' : 'link-blanco'}`}>Contacto</NavLink></li>
+            <li className="nav-item px-0 px-lg-4"><NavLink to='/' className={`nav-link ${scrolled ? 'link-celeste' : 'link-blanco'}`} onClick={handleNavLinkClick}>Inicio</NavLink></li>
+            <li className="nav-item px-0 px-lg-4"><NavLink to='/servicios' className={`nav-link ${scrolled ? 'link-celeste' : 'link-blanco'}`} onClick={handleNavLinkClick}>Servicios</NavLink></li>
+            <li className="nav-item px-0 px-lg-4"><NavLink to='/contacto' className={`nav-link ${scrolled ? 'link-celeste' : 'link-blanco'}`} onClick={handleNavLinkClick}>Contacto</NavLink></li>
           </ul>
         </div>
       </div>
